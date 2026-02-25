3f929dc55ae9e91ec66b39594647266bf49f25ef
Asanov <Asanov@DESKTOP-90N0PL6>
2026-02-25 15:15:29 +0600

    fix(infra): wait for leadsdb before starting leads api

--- InterStyle.AppHost/AppHost.cs
+++ InterStyle.AppHost/AppHost.cs
@@ -5,6 +5,6 @@ var postgres = builder.AddPostgres("postgres");
 var leadsDb = postgres.AddDatabase("leadsdb");
 
 builder.AddProject<Projects.InterStyle_Leads_Api>("interstyle-leads-api")
-    .WithReference(leadsDb);
+    .WithReference(leadsDb).WaitFor(leadsDb);
 
 builder.Build().Run();
